c5928deba0a9ec9d38e3215eecc99db51711d63f
Dolphin <cthukyaw13@gmail.com>
2021-10-30 22:39:17 +0630

    Update for group call

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = require("express")();
 const https = require('https');
 const cors = require("cors");
 
+const users = {};
+const socketToRoom = {};
+
 var key = fs.readFileSync('./cert/selfsigned.key', 'utf8');
 var cert = fs.readFileSync('./cert/selfsigned.crt', 'utf8');
 var options = {
@@ -31,6 +34,7 @@ app.get('/', (req, res) => {
 	res.send('Running');
 });
 
+/*
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
 
@@ -46,5 +50,44 @@ io.on("connection", (socket) => {
 		io.to(data.to).emit("callAccepted", data.signal)
 	});
 });
+*/
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
+
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
