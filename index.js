eba909b2a6d2396c87c60a20d3ad39f667a21767
Dolphin <cthukyaw13@gmail.com>
2021-11-08 17:42:50 +0630

    Some updates

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const fs = require('fs');
 const app = require("express")();
 const https = require('https');
+const http = require('http');
 const cors = require("cors");
 
-const users = {};
-const socketToRoom = {};
+let socketList = {};
 
 var key = fs.readFileSync('./cert/selfsigned.key', 'utf8');
 var cert = fs.readFileSync('./cert/selfsigned.crt', 'utf8');
@@ -15,6 +15,7 @@ var options = {
 };
 
 const server = https.createServer(options, app);
+//const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -34,60 +35,90 @@ app.get('/', (req, res) => {
 	res.send('Running');
 });
 
-/*
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
-});
-*/
-
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-
-        socket.emit("all users", usersInThisRoom);
-    });
-
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
-
+// Socket
+io.on('connection', (socket) => {
+    console.log(`New User connected: ${socket.id}`);
+  
     socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-        }
+      socket.disconnect();
+      console.log('User disconnected!');
     });
-
-});
+  
+    socket.on('BE-check-user', ({ roomId, userName }) => {
+      let error = false;
+  
+      io.sockets.in(roomId).clients((err, clients) => {
+        clients.forEach((client) => {
+          if (socketList[client] == userName) {
+            error = true;
+          }
+        });
+        socket.emit('FE-error-user-exist', { error });
+      });
+    });
+  
+    /**
+     * Join Room
+     */
+    socket.on('BE-join-room', ({ roomId, userName }) => {
+      // Socket Join RoomName
+      socket.join(roomId);
+      socketList[socket.id] = { userName, video: true, audio: true };
+  
+      // Set User List
+      io.sockets.in(roomId).clients((err, clients) => {
+        try {
+          const users = [];
+          clients.forEach((client) => {
+            // Add User List
+            users.push({ userId: client, info: socketList[client] });
+          });
+          socket.broadcast.to(roomId).emit('FE-user-join', users);
+          // io.sockets.in(roomId).emit('FE-user-join', users);
+        } catch (e) {
+          io.sockets.in(roomId).emit('FE-error-user-exist', { err: true });
+        }
+      });
+    });
+  
+    socket.on('BE-call-user', ({ userToCall, from, signal }) => {
+      io.to(userToCall).emit('FE-receive-call', {
+        signal,
+        from,
+        info: socketList[socket.id],
+      });
+    });
+  
+    socket.on('BE-accept-call', ({ signal, to }) => {
+      io.to(to).emit('FE-call-accepted', {
+        signal,
+        answerId: socket.id,
+      });
+    });
+  
+    socket.on('BE-send-message', ({ roomId, msg, sender }) => {
+      io.sockets.in(roomId).emit('FE-receive-message', { msg, sender });
+    });
+  
+    socket.on('BE-leave-room', ({ roomId, leaver }) => {
+      delete socketList[socket.id];
+      socket.broadcast
+        .to(roomId)
+        .emit('FE-user-leave', { userId: socket.id, userName: [socket.id] });
+      io.sockets.sockets[socket.id].leave(roomId);
+    });
+  
+    socket.on('BE-toggle-camera-audio', ({ roomId, switchTarget }) => {
+      if (switchTarget === 'video') {
+        socketList[socket.id].video = !socketList[socket.id].video;
+      } else {
+        socketList[socket.id].audio = !socketList[socket.id].audio;
+      }
+      socket.broadcast
+        .to(roomId)
+        .emit('FE-toggle-camera', { userId: socket.id, switchTarget });
+    });
+  });
 
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
